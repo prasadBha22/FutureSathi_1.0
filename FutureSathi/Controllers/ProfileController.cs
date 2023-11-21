165d564e2a4958a95ee55eb0a3a2c641a7f667cc
Shubham234 <Shubham234@DESKTOP-RMFLQF5>
2023-11-21 21:55:00 +0530

    Profile code

---
+++ FutureSathi/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using FutureSathi.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FutureSathi.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        FutureSathiEntities ctx = new FutureSathiEntities();
+        IUser m ;
+        public ProfileController(IUser _m)
+        {
+            m = _m;
+        
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public ActionResult GetUser()
+        {
+          string Ui =User.Identity.Name.ToString();
+            var obj = m.GetAllUserInfo(Ui);
+
+            return Json(obj,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
